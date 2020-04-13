a91d9f10ad41ec362cf3d3f68f78e53783909c63
Yusuke Ueda <418810@m.mie-u.ac.jp>
2020-04-13 19:41:50 +0900

    created User model

--- account/models.py
+++ account/models.py
@@ -1,5 +1,66 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import (BaseUserManager, AbstractBaseUser)
 
-class User(AbstractUser):
-    pass
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, date_of_birth, height, weight, pace_of_losing_weight, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            date_of_birth=date_of_birth,
+            height=height,
+            weight=weight,
+            pace_of_losing_weight=pace_of_losing_weight,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, date_of_birth, password):
+        user = self.create_user(
+            email,
+            password=password,
+            date_of_birth=date_of_birth,
+            height=170,
+            weight=60,
+            pace_of_losing_weight=1,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name='email',
+        max_length=255,
+        unique=True,
+    )
+    date_of_birth = models.DateField()
+    height = models.IntegerField()
+    weight = models.IntegerField()
+    # between 1 and 5
+    pace_of_losing_weight = models.IntegerField(default=3)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['date_of_birth']
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
