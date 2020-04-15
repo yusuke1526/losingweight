adcac7d95ef24acc4fe5ddf2bafce7fdbff26f68
Yusuke Ueda <418810@m.mie-u.ac.jp>
2020-04-15 17:55:52 +0900

    some fixes

--- diet/models.py
+++ diet/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.conf import settings
+from django.utils import timezone
 
 class Diet(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    date = models.DateField()
+    date = models.DateField(default=timezone.now)
     name = models.CharField(max_length=100)
     calorie = models.IntegerField()
 
--- diet/views.py
+++ diet/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from account.models import User
+from accounts.models import User
 
 def home(request):
     return render(request, 'diet/home.html')
 
-def userpage(request, user_id):
-    # the user who have the userpage
-    host = get_object_or_404(User, pk=user_id)
-    return render(request, 'diet/userpage.html', {'host': host})
+def userpage(request):
+    return render(request, 'diet/userpage.html')
